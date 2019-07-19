eda715291fb6d69c6ccb0f8d8a14e836001dd579
ppichier <pierantonio.pichierri@gmail.com>
2019-07-19 16:05:08 +0200

    [REFACT] global

--- index.js
+++ index.js
@@ -1,52 +1,12 @@
-require("express-async-errors");
 const winston = require("winston");
-require("winston-mongodb");
-const config = require("config");
-const Joi = require("joi");
-Joi.objectId = require("joi-objectid")(Joi);
-const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 
+require("./startup/logging")(); // logging module first if error in the next 2 lines
 require("./startup/routes")(app);
-
-//for caughting exception if it s outside the context of express (pipeline);
-/* process.on("uncaughtException", ex => {
-  console.log("WE GOT AN UNCAUGHT EXCEPTION");
-  winston.error(ex.message, ex);
-  process.exit(1);
-});
- */
-winston.handleExceptions(
-  new winston.transports.File({ filename: "uncaughtExceptions.log" })
-);
-
-process.on("unhandledRejection", ex => {
-  throw ex;
-});
-
-//for caughting unhandled rejection (promises)
-/* process.on("unhandledRejection", ex => {
-  console.log("WE GOT AN UNHANDLED REJECTION");
-  winston.error(ex.message, ex);
-  process.exit(1);
-}); */
-
-winston.add(new winston.transports.File({ filename: "logfile.log" }));
-winston.add(
-  new winston.transports.MongoDB({ db: "mongodb://localhost/vidly" })
-);
-
-if (!config.get("jwtPrivateKey")) {
-  console.error("FATAL ERROR: jwt private key is not defibed");
-  process.exit(1);
-}
-
-mongoose
-  .connect("mongodb://localhost/vidly", { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB - vidly db ..."))
-  .catch(err => console.error("Could not connect to MongoDB."));
-
+require("./startup/db")();
+require("./startup/config")();
+require("./startup/validation")();
 
 app.get("/", (req, res) => {
   res.send("Hello world !");
@@ -54,4 +14,4 @@ app.get("/", (req, res) => {
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => winston.info(`Listening on port ${port}`));
---
+++ startup/logging.js
@@ -0,0 +1,33 @@
+const winston = require("winston");
+require("winston-mongodb");
+require("express-async-errors");
+
+module.exports = function() {
+  //for caughting exception if it s outside the context of express (pipeline);
+  /* process.on("uncaughtException", ex => {
+  console.log("WE GOT AN UNCAUGHT EXCEPTION");
+  winston.error(ex.message, ex);
+  process.exit(1);
+});
+ */
+  winston.handleExceptions(
+    new winston.transports.Console({ colorize: true, prettyPrint: true }),
+    new winston.transports.File({ filename: "uncaughtExceptions.log" })
+  );
+
+  process.on("unhandledRejection", ex => {
+    throw ex;
+  });
+
+  //for caughting unhandled rejection (promises)
+  /* process.on("unhandledRejection", ex => {
+    console.log("WE GOT AN UNHANDLED REJECTION");
+    winston.error(ex.message, ex);
+    process.exit(1);
+  }); */
+
+  winston.add(new winston.transports.File({ filename: "logfile.log" }));
+  winston.add(
+    new winston.transports.MongoDB({ db: "mongodb://localhost/vidly" })
+  );
+};
